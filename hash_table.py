0cfb5d677f781a1d024d37af44ca07d7fd5d4df0
Aaron Kummer <aaronjkummer@gmail.com>
2021-07-16 12:37:00 -0400

    changes HashTable

--- hash_table.py
+++ hash_table.py
@@ -1,83 +1,38 @@
-class HashTable(object):
-     
-    def __init__(self,size):
-         
-        # We set up size and slots and data
-        self.size = size
-        self.slots = [None] * self.size
-        self.data = [None] * self.size
-         
-    def put(self,key,data):
-         
-        # We get the hash value
-        hashvalue = self.hashfunction(key,len(self.slots))
- 
-        # in case Slot is Empty
-        if self.slots[hashvalue] == None:
-            self.slots[hashvalue] = key
-            self.data[hashvalue] = data
-         
+class HashTable:
+
+    def __init__(self) -> None:
+        self.size = 10
+        self.map = [None] * self.size
+
+    def get(self,k):
+        key = self.hash(k)
+        slot = self.map[key]
+        for kv in slot:
+            k,v = kv
+            if key == k:
+                return v
+
+    def set(self, key, value):
+        key_hash = self.hash(key)
+        key_value = [key, value]
+
+        if self.map[key_hash] == None:
+            self.map[key_hash] = list([key_value])
+            return True
         else:
-             
-            # If the key already exists, replace old value
-            if self.slots[hashvalue] == key:
-                self.data[hashvalue] = data  
-             
-            # Otherwise, find the next available slot - collision case
-            else:
-                 
-                nextslot = self.rehash(hashvalue,len(self.slots))
-                 
-                # Get to the next slot
-                while self.slots[nextslot] != None and self.slots[nextslot] != key:
-                    nextslot = self.rehash(nextslot,len(self.slots))
-                 
-                # Set new key, if NONE
-                if self.slots[nextslot] == None:
-                    self.slots[nextslot]=key
-                    self.data[nextslot]=data
-                     
-                # Otherwise replace old value
-                else:
-                    self.data[nextslot] = data 
- 
-    def hashfunction(self,key,size):
-        # Remainder Method
-        return key%size
- 
-    def rehash(self,oldhash,size):
-        # For finding next possible positions
-        return (oldhash+1)%size
-     
-     
-    def get(self,key):
-         
-        # Get the  items given a key
-         
-        # Set up variables for our search
-        startslot = self.hashfunction(key,len(self.slots))
-        data = None
-        stop = False
-        found = False
-        position = startslot
-         
-        # Until we discern that its not empty or found (and haven't stopped yet)
-        while self.slots[position] != None and not found and not stop:
-             
-            if self.slots[position] == key:
-                found = True
-                data = self.data[position]
-                 
-            else:
-                position=self.rehash(position,len(self.slots))
-                if position == startslot:
-                     
-                    stop = True
-        return data
- 
-    # Special Methods for use with Python indexing
-    def __getitem__(self,key):
-        return self.get(key)
- 
-    def __setitem__(self,key,data):
-        self.put(key,data)
+            for pair in self.map[key_hash]:
+                if pair[0] == key:
+                    pair[1] = key_value
+                    return True
+            self.map[key_hash].append(key_value)
+            return True
+
+    def hash(self, key) -> int:
+        return hash(key) % self.size 
+
+
+H = HashTable()
+H.set('a', "2530")
+H.set(3, "233 Canyon Rd,Salt Lake City,UT,84103,EOD,2,Can only be on truck 2")
+
+print(H.map)
