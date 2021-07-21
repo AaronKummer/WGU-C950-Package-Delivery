3e53d4e21667b0945f07b7fa86fb7c6581b47a95
Aaron Kummer <aaronjkummer@gmail.com>
2021-07-21 16:53:45 -0400

    begins calculations for packages

--- delivery_truck.py
+++ delivery_truck.py
@@ -2,7 +2,9 @@ class DeliveryTruck:
     distance = 0
     packages = []
     priority_packages = []
+    current_address_index = 0
 
     def __init__(self) -> None:
         self.packages = []
         self.priority_packages = []
+        self.at_home = True
--- package_delivery_service.py
+++ package_delivery_service.py
@@ -6,12 +6,15 @@ from delivery_truck import DeliveryTruck
 class PackageDeliveryService: 
     truck_speed = 18/60
     package_data = HashTable()
-    packages = []
+    priority_packages = []  #these packages have to be delievered at a special time
+    grouped_packages = []   #these packages have to be delivered with another package
     truck_one = DeliveryTruck()
     truck_two = DeliveryTruck()
     truck_three = DeliveryTruck()
 
     def __init__(self) -> None:
+        self.addresses = list(csv.reader(open('data/addresses.csv'), delimiter=','))
+        self.distances = list(csv.reader(open('data/distances.csv'), delimiter=','))
         self.get_package_data()
         self.load_trucks()
         self.begin_deliveries(self.truck_one)
@@ -27,18 +30,41 @@ class PackageDeliveryService:
     # arranges data based on the notes and grouped packages from the csv and places package ids into trucks
     def load_trucks(self) -> None:
         self.truck_one.packages = [13,14,15,16,29,30,31,34,37,40,1,4,7,39,8,32]
-        self.truck_two.packages = [3,18,36,38,6,10,11,12,17,20,21,22,23,24,25,26]
-        self.truck_three.packages = [9,19,27,28,33,35,2,5]
-        
-        self.truck_one.priority_packages = [13,14,15,16,29,30,31,34,37,40]
-        self.truck_two.priority_packages = [6,20,25]
-        self.truck_three.priority_packages = []
 
+    # O(n)
+    def get_column(self,matrix, i):
+        return [row[i] for row in matrix]
+
+    def get_package_distance(self,package:DeliveryPackage, truck: DeliveryTruck) -> float:
+        # print("getting distance for ", package.address)
+        address_id = 0
+        for address in self.addresses:
+            if (address[2] == package.address):
+                address_id = address[0]
+
+        # This column will have distance information for the current address the truck is at
+        distance_column = self.get_column(self.distances, truck.current_address_index) 
+        return distance_column[int(address_id)]
+        # [0] will be the current location's distance from itself which is 0
+        # next_closest_distance = sorted(distance_column)[1]
+        # next_closest_address_index = distance_column.index(next_closest_distance)
+
+    def find_next_package_to_deliver(self,truck: DeliveryTruck):
+        print("finding next package")
+        distances_of_packages_on_truck = []
+        for package_index in truck.packages: 
+            distance = self.get_package_distance(DeliveryPackage(package_index, *self.package_data.get(package_index)), truck)
+            distances_of_packages_on_truck.append(distance)
+        print(distances_of_packages_on_truck)
+        
+
+    def deliver_package(self,truck,package):
+        print("delivering package")
 
     # Entrance to main delivery alrogrithm. Loops through packages for each truck and finds the next closest delivery
     def begin_deliveries(self, truck: DeliveryTruck) :
-        for package in truck.packages:
-            print(package)
+        next_package = self.find_next_package_to_deliver(truck)
+            
 
 
 
